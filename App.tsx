9066ebfeed9b2ed4abbcfb5decf45509217ce75c
tuomasthuren <95663440+tuomasthuren@users.noreply.github.com>
2026-04-06 22:09:20 +0300

    Initial commit: Complete workout tracker app with navigation, screens, and local storage

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Add your screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
